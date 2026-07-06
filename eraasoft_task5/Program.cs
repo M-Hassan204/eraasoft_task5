890b3eded125205da0133ec971bd9dd9ece5ab75
moe's Pc <m.hassan.dev@icloud.com>
2026-07-06 10:24:45 +0300

    Add project files.

---
+++ eraasoft_task5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace eraasoft_task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
